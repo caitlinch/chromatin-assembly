75b4cf27e3dfc30962528631c16eef8c59a4cf9d
Caitlin Cherryh <24237077+caitlinch@users.noreply.github.com>
2024-06-27 18:18:17 +1000

    Getting graph done

--- workflow/slurm_snakemake.sh
+++ workflow/slurm_snakemake.sh
@@ -2,11 +2,11 @@
 
 # Usage: sbatch workflow/slurm_snakemake.sh
 
-#SBATCH --job-name=snakemaketut
-#SBATCH --time=00:10:00 # 10 minutes
-#SBATCH --mem=100KB # 0.1 MB, 0.0001 GB
+#SBATCH --job-name="1_repeatmasker"
+#SBATCH --time=2-00:00:00 # 2 days
+#SBATCH --mem=80GB
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=20
 
 # one may use %j instead of %A in below
 #SBATCH --error=/scratch3/che318/snakemake_tut/results/smt_%A_%a.err
@@ -15,6 +15,5 @@
 module load python
 cd /scratch3/che318/snakemake_tut/
 snakemake -s workflow/Snakefile --reason --printshellcmds /scratch3/che318/snakemake_tut/results/b_a.txt
-
 
 
