2ec2e107ecd13571fe68fca1cbe4322a37b3ea71
Caitlin Cherryh <24237077+caitlinch@users.noreply.github.com>
2025-06-12 12:07:13 +1000

    Separate Steps 3-5 into individual job scripts

---
+++ jobscripts/templates/slurm_pipeline_S5.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Usage: sbatch slurm_pipeline_S5.sh
+
+# NOTE: Depending on your data, you may need to update the resources in this job script or
+#       in the Snakemake slurm profile (chromatin-assembly/profiles/slurm/config.yaml)
+
+#SBATCH --job-name=CA_S5
+#SBATCH --partition=ext
+#SBATCH --time=25-00:00:00
+#SBATCH --mem=5MB # memory for Snakemake - not memory required for individual pipeline steps
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=caitlin.cherryh@csiro.au
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+#SBATCH --account=OD-233464
+#SBATCH --error=/scratch3/che318/chromatin-assembly/logs/slurm_%j_%x.err
+#SBATCH --out=/scratch3/che318/chromatin-assembly/logs/slurm_%j_%x.out
+
+# ----------------Modules------------------------- #
+module load python
+
+# ---------------- Pipeline Steps ------------------- #
+# Assuming that output from Steps 0, 1, 3 and 4 is present, run:
+#       Step 5 - Remove PCR and optical duplicates from alignment with Picard
+snakemake --slurm --profile profiles/slurm/ --until s5_alignment_deduplication --omit s2_raw_read_QC,s6a_two_bit_genome,s6b_effective_genome_size
